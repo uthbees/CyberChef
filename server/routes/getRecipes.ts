bab73916fb63a7018a8c22703ecb636a10169472
patoneill24 <poneill33@byui.edu>
2024-11-01 15:24:14 -0600

    Update getRecipes.ts

--- server/routes/getRecipes.ts
+++ server/routes/getRecipes.ts
@@ -1,6 +1,8 @@
 import { RequestHandler } from 'express';
+import { openDb } from '../utils/openDb';
 
-const getRecipes: RequestHandler = (req, res) => {
+const getRecipes: RequestHandler = async (req, res) => {
+    const db = await openDb();
     res.send('stub implementation');
 };
 
