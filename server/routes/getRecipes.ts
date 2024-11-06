daae8a24f9e48ce2ded856442d2f139f3200594f
patoneill24 <poneill33@byui.edu>
2024-11-06 14:47:14 -0700

    added coors and get function

--- server/routes/getRecipes.ts
+++ server/routes/getRecipes.ts
@@ -2,8 +2,16 @@ import { RequestHandler } from 'express';
 import { openDb } from '../utils/openDb';
 
 const getRecipes: RequestHandler = async (req, res) => {
-    const db = await openDb();
-    res.send('stub implementation');
+    try {
+        const db = await openDb();
+        const rows = await db.all('SELECT * FROM recipes');
+        // console.log('Recipes loaded successfully');
+        res.status(200);
+        res.json(rows);
+    } catch (err) {
+        console.error('Error loading recipes', err);
+        res.status(500);
+    }
 };
 
 export default getRecipes;
--- server/routes/postRecipes.ts
+++ server/routes/postRecipes.ts
@@ -1,6 +1,16 @@
 import { RequestHandler } from 'express';
 
 const postRecipes: RequestHandler = (req, res) => {
+    // try{
+    //     const db = await openDb();
+    //     await db.run('INSERT INTO recipes VALUES (?,?,?,?,?,?,?)','cookies',30,60,'Easy',['make dough','put dough on cooking sheet','put in oven','enjoy'],'ad3d4678-857a-499d-8a60-3c1d65836b7c');
+    //     res.status(201).json({message: 'Recipe added successfully'});
+    //     console.log('Recipe added successfully');
+    // } catch (err) {
+    //     console.error('Error inserting recipe', err);
+    //     res.status(500);
+    //     console.log('Error inserting recipe');
+    // }
     res.send('stub implementation');
 };
 
