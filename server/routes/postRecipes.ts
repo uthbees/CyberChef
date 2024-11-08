cf5d94de9290930efb4d7d8c31a2983344d17b64
patoneill24 <poneill33@byui.edu>
2024-11-08 12:02:17 -0700

    Update postRecipes.ts

--- server/routes/postRecipes.ts
+++ server/routes/postRecipes.ts
@@ -1,9 +1,10 @@
 import { RequestHandler } from 'express';
+// import { openDb } from '../utils/openDb';
 
 const postRecipes: RequestHandler = (req, res) => {
     // try{
     //     const db = await openDb();
-    //     await db.run('INSERT INTO recipes VALUES (?,?,?,?,?,?,?)','cookies',30,60,'Easy',['make dough','put dough on cooking sheet','put in oven','enjoy'],'ad3d4678-857a-499d-8a60-3c1d65836b7c');
+    //     await db.run('INSERT INTO recipes VALUES ("cookies",30,60,"Easy",["make dough","put dough on cooking sheet","put in oven","enjoy"],"ad3d4678-857a-499d-8a60-3c1d65836b7c")');
     //     res.status(201).json({message: 'Recipe added successfully'});
     //     console.log('Recipe added successfully');
     // } catch (err) {
