3a83eb5720dd36168f1cd1a6b46809d1e4bf61ae
patoneill24 <poneill33@byui.edu>
2024-11-11 14:24:17 -0700

    update db

--- server/routes/getRecipes.ts
+++ server/routes/getRecipes.ts
@@ -4,8 +4,13 @@ import { openDb } from '../utils/openDb';
 const getRecipes: RequestHandler = async (req, res) => {
     try {
         const db = await openDb();
-        const rows = await db.all('SELECT * FROM recipes');
+        const rows = await db.all(
+            'SELECT * FROM recipes, ingredients, instructions, recipes_has_ingredients',
+        );
         // console.log('Recipes loaded successfully');
+        // rows = await db.all('SELECT * FROM ingredients');
+        // rows += db.all('SELECT * FROM instructions');
+        // rows += db.all('SELECT * FROM recipes_has_ingredients');
         res.status(200);
         res.json(rows);
     } catch (err) {
