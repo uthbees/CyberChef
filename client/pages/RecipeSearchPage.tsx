7bd62fb7f55cce9728b365ad3e971d56312a3039
Ethan Ball <uthbees@gmail.com>
2024-11-12 07:43:55 -0700

    Show recipes on recipe search page

--- client/pages/RecipeSearchPage.tsx
+++ client/pages/RecipeSearchPage.tsx
@@ -1,3 +1,10 @@
+import { useContext } from 'react';
+import { AllRecipesContext } from '../App/AllRecipesContextProvider';
+
 export default function RecipeSearchPage() {
-    return 'stub implementation - recipe search';
+    const { recipes } = useContext(AllRecipesContext);
+
+    return Object.values(recipes).map((r, index) => (
+        <p key={index}>{r.name}</p>
+    ));
 }
